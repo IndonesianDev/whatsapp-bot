766efdc6e3318613e2ae7a96d3b3443b7d89fcbc
alpio adjijanuar <68207798+AlvioAdjiJanuar@users.noreply.github.com>
2021-04-05 12:47:34 +0700

    UPDATE PREMIUM PAKE CODE KALO ERROR BUAT ISSUE

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const moment = require('moment-timezone')
+const crypto = require('crypto')
 moment.tz.setDefault('Asia/Jakarta').locale('id')
 
 /**
@@ -27,6 +28,13 @@ const processTime = (timestamp, now) => {
     // timestamp => timestamp when message was received
     return moment.duration(now - moment(timestamp * 1000)).asSeconds()
 }
+/**
+ * Get Code For Premium
+ */
+const createcode = (size) => {
+    return crypto.randomBytes(size).toString('hex').slice(0, size)
+}
+
 
 /**
  * is it url?
@@ -64,6 +72,7 @@ module.exports = {
         addFilter
     },
     processTime,
+    createcode,
     isUrl,
     color,
     messageLog
