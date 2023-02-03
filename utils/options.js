02bccae84582b31b581dabd6894f4603d854cc70
Piyoxz <102013178+Piyoxz@users.noreply.github.com>
2023-02-03 17:03:02 +0700

    Update Stuck After running

--- utils/options.js
+++ utils/options.js
@@ -8,13 +8,11 @@ module.exports = options = (headless, start) => {
     const options = {
         sessionId: 'piyobot',
         headless: headless,
+        multiDevice: true,
         qrTimeout: 0,
         authTimeout: 0,
         restartOnCrash: start,
         cacheEnabled: false,
-        useChrome: true,
-        killProcessOnBrowserClose: true,
-        throwErrorOnTosBlock: false,
         chromiumArgs: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
